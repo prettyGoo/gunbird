64241fff21f7304fb660a4e47d4e6157086fa917
Alexei Kostyuchenko <kos_ae@outlook.com>
2017-11-19 16:37:47 +0700

    add shot function

---
+++ app.js
@@ -0,0 +1,36 @@
+var program = require('commander');
+
+program
+  .version('0.1.0')
+  .usage('[options] <file ...>')
+  .option('-s, --sparrow <n>', 'A sparrow velocity', parseFloat)
+  .option('-x, --x <n>', 'A range', parseFloat)
+  .option('-y, --y <n>', 'A list', parseFloat)
+  .option('-b, --bullet <n>', 'A bullet velocity', parseFloat)
+  .option('-w, --wind', 'A wind velocity', parseFloat)
+  .parse(process.argv);
+
+ function shot(s_velocity, height, distance, b_velocity, w_velocity) {
+  if (distance <= 0) {
+    return -1; // Воробей сидит на земле 
+         // (по отдыхающим воробьям не стреляем) 
+         // или летит под землёй!!!
+   }
+  
+   if (b_velocity <= 0) {
+    return -2; // Порох промок!!!
+   }
+  
+   let angle = (Math.asin(-(s_velocity * distance) / (b_velocity * Math.sqrt(Math.pow(height, 2) + Math.pow(distance, 2))))
+     + Math.acos(height / Math.sqrt(Math.pow(height, 2) + Math.pow(distance, 2)))) * (360 / (2 * Math.PI));
+
+   if (!isNaN(angle) && angle > 0) {
+    return angle;
+   }
+  
+   return -3; // Воробьи летают слишком быстро!!!
+ }
+
+ let angle = shot(program.sparrow, program.x, program.y, program.bullet, program.wind);
+
+ console.log('The angle is ' + angle);
